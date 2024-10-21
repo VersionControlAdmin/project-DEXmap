148714f66a48be9181709b4e596aa7114ebaf1a9
Sebastian Luft <sebastian.luft.gm@gmail.com>
2024-10-21 14:59:50 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello, Tailwind CSS with Vite and React!</h1>
+    </div>
+  );
+}
+
+export default App
